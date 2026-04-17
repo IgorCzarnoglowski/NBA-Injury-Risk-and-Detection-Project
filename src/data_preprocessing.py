cb62fc2eff4fea5313f6ed6b6230f5423174f1ff
DjCr4y0n <igor.czarnoglowski.1ap@gmail.com>
2026-04-17 16:36:44 +0200

    ad comments

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -8,8 +8,9 @@ def remove_duplicates(df: pd.DataFrame):
     return df
 
 def remove_unused_columns(df: pd.DataFrame):
+    # Removing unused columns in gamelogs dataset
     print('Removing unused columns...')
-    unused_columns = ['SEASON_ID', 'VIDEO_AVAILABLE', 'PLAYER_ID', 'FG3_PCT', 'FG_PCT', 'FT_PCT']
+    unused_columns = ['SEASON_ID', 'VIDEO_AVAILABLE', 'FG3_PCT', 'FG_PCT', 'FT_PCT']
 
     for c in unused_columns:
         if c in df.columns:
@@ -19,12 +20,14 @@ def remove_unused_columns(df: pd.DataFrame):
     return df
 
 def join_positions(df: pd.DataFrame):
+    # Join positions in Injury dataset
     print('Joining guards and forwards positions...')
 
     df["Position"] = df["Position"].replace({'PG': 'G', 'SG': 'G', 'SF': 'F', 'PF': 'F'})
     return df
 
 def convert_data_column(df: pd.DataFrame):
+    # Converting to the same data format, as a primary data format we use the on in injury_history
     print('Converting time column to the same format...')
 
     df['GAME_DATE'] = pd.to_datetime(df['GAME_DATE']).dt.strftime('%d/%m/%Y')
@@ -36,7 +39,7 @@ def rename_columns(df:pd.DataFrame, name: Literal['stats', 'injuries']):
     df.columns = df.columns.str.lower()
     if name == 'stats':
         df.rename(columns={
-            'wl': 'result'
+            'wl': 'match_result'
         }, inplace=True)
 
     return df
