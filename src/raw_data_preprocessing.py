10e9d2ae0fc2054d65d1b2a5d5de0053cc0b5285
DjCr4y0n <igor.czarnoglowski.1ap@gmail.com>
2026-04-17 17:57:18 +0200

    add preprocessing process for raw data

--- src/raw_data_preprocessing.py
+++ src/raw_data_preprocessing.py
@@ -8,23 +8,23 @@ def remove_duplicates(df: pd.DataFrame):
 
     return df
 
+def join_positions(df: pd.DataFrame):
+    # Join positions in Injury dataset
+    print('Joining guards and forwards positions...')
+
+    df["Position"] = df["Position"].replace({'PG': 'G', 'SG': 'G', 'SF': 'F', 'PF': 'F'})
+    return df
+
 def remove_unused_columns(df: pd.DataFrame):
     # Removing unused columns in gamelogs dataset
     print('Removing unused columns...')
-    unused_columns = ['SEASON_ID', 'VIDEO_AVAILABLE', 'FG3_PCT', 'FG_PCT', 'FT_PCT', 'PLUS_MINUS']
+    unused_columns = ['VIDEO_AVAILABLE', 'FG3_PCT', 'FG_PCT', 'FT_PCT']
 
     for c in unused_columns:
         if c in df.columns:
             df.drop(c, axis=1, inplace=True)
             print(f'Removed: {c}')
 
-    return df
-
-def join_positions(df: pd.DataFrame):
-    # Join positions in Injury dataset
-    print('Joining guards and forwards positions...')
-
-    df["Position"] = df["Position"].replace({'PG': 'G', 'SG': 'G', 'SF': 'F', 'PF': 'F'})
     return df
 
 def convert_data_column(df: pd.DataFrame):
@@ -35,6 +35,7 @@ def convert_data_column(df: pd.DataFrame):
     return df
 
 def rename_columns(df:pd.DataFrame, name: Literal['stats', 'injuries']):
+    print(f'Renaming column names for {name} dataset...')
     if name not in ('stats', 'injuries'):
         raise ValueError(f'Invalid name. Choose from: stats, injuries')
     df.columns = df.columns.str.lower()
@@ -42,16 +43,24 @@ def rename_columns(df:pd.DataFrame, name: Literal['stats', 'injuries']):
         df.rename(columns={
             'wl': 'match_result'
         }, inplace=True)
-
+    elif name == 'injuries':
+        df.rename(columns={
+            'name': 'player_name',
+            'date': 'injury_date',
+            'notes': 'injury_description'
+        }, inplace=True)
     return df
 
 def map_game_host(df:pd.DataFrame):
+    print('Mapping hosts to the game...')
+
     df['host_team'] = df['matchup'].apply(
         lambda x: x[-3:] if '@' in x else x[:3]
     )
     return df
 
 def map_player_with_id(df: pd.DataFrame):
+    print('Mapping id to the players...')
     for idx, name in df['Name'].items():
         player_id = get_player_id(name)
         df.at[idx, 'Player_id'] = player_id
@@ -64,6 +73,29 @@ def map_player_with_id(df: pd.DataFrame):
 
 
 if __name__ == '__main__':
-    df = load_raw_data()['injuries']
-    df = map_player_with_id(df)
-    print(df.head(70))
+    pd.set_option('display.max_columns', 30)
+    injury_df = load_raw_data()['injuries']
+    injury_df = remove_duplicates(injury_df)
+    injury_df = join_positions(injury_df)
+    injury_df = map_player_with_id(injury_df)
+    injury_df = rename_columns(injury_df, 'injuries')
+    injury_df['injury_id'] = range(len(injury_df))
+    injury_df = injury_df.iloc[:, [6, 5, 0, 1, 2, 3, 4]]
+    
+    injury_df.to_csv('Injury_History_Preprocessed.csv')
+
+    gamelog_df = load_raw_data()['stats']
+    gamelog_df = remove_duplicates(gamelog_df)
+    gamelog_df = remove_unused_columns(gamelog_df)
+    gamelog_df = convert_data_column(gamelog_df)
+    gamelog_df = rename_columns(gamelog_df, 'stats')
+    gamelog_df = map_game_host(gamelog_df)
+    gamelog_df['stats_id'] = range(len(gamelog_df))
+    front_cols = ['stats_id', 'game_id', 'season_id', 'player_id', 'host_team']
+    remaining = [c for c in gamelog_df.columns if c not in front_cols]
+    gamelog_df = gamelog_df[front_cols + remaining]
+
+
+    gamelog_df.to_csv('Gamelogs_2014-2020_Preprocessed.csv')
+
+
